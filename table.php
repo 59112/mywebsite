6bc1f8578020acbea0e03836aa0edc238dedb0cf
59112 <97937543+59112@users.noreply.github.com>
2022-02-10 22:53:36 +0700

    Update table.php

--- table.php
+++ table.php
@@ -14,44 +14,44 @@
                         <tr>
                                 <td>1</td>
                                 <td>KIM HANBIN</td>
-                                <td>ฮันบิน</td>
-                                <td><imgsrc="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.nme.com%2Fen_asia%2Fnews%2Fmusic%2Fikon-b-i-sentence-four-year-probation-jail-drug-case-scandal-3042448&psig=AOvVaw0fKi5mKhAozDslsPWw4JsJ&ust=1644592091051000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKCwhZO19fUCFQAAAAAdAAAAABAR" width="130px"></td>
+                                <td>ต๋าของหม่าม้า</td>
+                                <td><img src="https://i.pinimg.com/originals/96/92/1a/96921a39c1f835eb4c8388ce413ecfda.jpg" width="130px"></td>
                                 <td><a href="https://twitter.com/shxx131bi131?s=21">twitter</a></td>
                         </tr>
                         <tr>
                                 <td>2</td>
                                 <td>PARK CHAEYOUNG</td>
-                                <td>เนย</td>
-                                <td><img src="https://www.thebangkokinsight.com/wp-content/uploads/2021/02/batch_113-1.jpg" width="140px"></td>
-                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                                <td>นางสาวโรซี่</td>
+                                <td><img src="https://pkpkgirls.files.wordpress.com/2020/06/200626-blackpink-hylt-melon_1.jpg" width="140px"></td>
+                                <td><a href="https://twitter.com/blackpink?s=21">twitter</a></td>
                         </tr>
                         <tr>
                                 <td>3</td>
-                                <td>พัศชนันท์ เจียจิรโชติ</td>
-                                <td>อร</td>
-                                <td><img src="https://www.online-idol.com/wp-content/uploads/2018/02/p-%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B1%E0%B8%95%E0%B8%B4-%E0%B8%AD%E0%B8%A3%E0%B8%AD%E0%B8%B8%E0%B9%8B%E0%B8%87-%E0%B8%AD%E0%B8%A3-BNK48.jpg" width="140px"></td>
-                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                                <td>KIM JENNIE</td>
+                                <td>ยัยเกี๊ยว</td>
+                                <td><img src="https://i.pinimg.com/564x/44/0b/b8/440bb86d12d52d74e944271617a6d12b.jpg" width="140px"></td>
+                                <td><a href="https://twitter.com/blackpink?s=21">twitter</a></td>
                         </tr>
                         <tr>
                                 <td>4</td>
-                                <td>ณัฐทิชา จันทรวารีเลขา</td>
-                                <td>ฟ้อนต์</td>
-                                <td><img src="https://thailandsuperstar.com/images/pix/7613280384/T_2256080949653329982.jpg" width="130px"></td>
-                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                                <td>KIM JIWON</td>
+                                <td>คุณบ๊อบบี้</td>
+                                <td><img src="http://res.heraldm.com/content/image/2021/01/25/20210125000846_0.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/yg_ikonic?s=21">twitter</a></td>
                         </tr>
                         <tr>
                                 <td>5</td>
-                                <td>ปัญสิกรณ์ ติยะกร</td>
-                                <td>ปัญ</td>
-                                <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-02-Feb/pun_05.jpg" width="130px"></td>
-                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                                <td>WANATABE HARUTO</td>
+                                <td>ฮารุโตะ</td>
+                                <td><img src="https://img.kpopmap.com/780x0/2021/12/treasure-haruto-visuals.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/treasuremembers?s=21">twitter</a></td>
                         </tr>
                         <tr>
                                 <td>6</td>
-                                <td>เจนนิษฐ์ โอ่ประเสิรฐ</td>
-                                <td>เจนนิษฐ์</td>
-                                <td><img src="https://f.ptcdn.info/833/073/000/quqnns11ab27EjGGcDfp-o.jpg" width="130px"></td>
-                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                                <td>KOO JUNHOE</td>
+                                <td>เด็กชายจุนเน่ง</td>
+                                <td><img src="https://i.pinimg.com/564x/67/97/22/6797229ab1344538fb1a7dea61e6eeea.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/yg_ikonic?s=21">twitter</a></td>
                         </tr>
         </body>
 </html>
