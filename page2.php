fc4a66e28d2a2ab4618a2a896f11b5561064bcea
59112 <97937543+59112@users.noreply.github.com>
2022-02-22 11:04:24 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title> css </title>
+		<style>
+			body{
+				background-color: ffa0ff;
+			}
+			#box1{
+				width: 800px;
+				height: 350px;
+				color: ff5;
+				background-color: c8ecdc;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="box1">
+			<h1> Paparwee Sungthong 28 4/13 </h1>
+		</div>
+	</body>
+</html>
