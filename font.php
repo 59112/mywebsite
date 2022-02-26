933a22afac41eff72c0009897220b12127294b76
59112 <97937543+59112@users.noreply.github.com>
2022-02-26 16:42:47 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(ChocolateteehaypaiTEST.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(smile-Regular.woff)
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวปภาวี สังข์ทอง ม.4/13 เลขที่ 28 เลขประจำตัว 59112</h1>
+  <h1 id="font2">นางสาวปภาวี สังข์ทอง ม.4/13 เลขที่ 28 เลขประจำตัว 59112</h1>
+</body>
+</html>
